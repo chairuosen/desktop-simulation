32d11449c053a04788c19fb9274b9abcb7551c16
ruosen.chai <chairuosen@gmail.com>
2016-06-15 11:21:47 +0800

    update app class

--- src/service/app.js
+++ src/service/app.js
@@ -27,69 +27,68 @@ function App(o) {
     $.extend(this,_default);
 
     this.sortKey = count++;
+}
 
-    this.hide = function () {
-        this.show = false;
-        this.actived = false;
-    };
+App.prototype.set = function (key,value) {
+    var isResize = ['height','width'].indexOf(key)>=0;
+    if(isResize){
+        this.maximized = false;
 
-    this.close = function () {
-        this.closed = true;
-        $event.emit('app:close',this);
-    }
-
-    this.maximize = function () {
-        var _this = this;
-        if(_this.maximized && _this.oldPosition){
-            _this.animating = true;
-            delay(function () {
-                _this.maximized = false;
-
-                _this.top = _this.oldPosition.top;
-                _this.left = _this.oldPosition.left;
-                _this.height = _this.oldPosition.height;
-                _this.width = _this.oldPosition.width;
-
-                _this.oldPosition = null;
-                delay(function () {
-                    _this.animating = false;
-                },500)
-            })
-
-        }else{
-            _this.animating = true;
-            delay(function () {
-                _this.maximized = true;
-                _this.oldPosition = {
-                    top:_this.top,
-                    left:_this.left,
-                    height:_this.height,
-                    width:_this.width
-                };
-                _this.top = 0;
-                _this.left = 0;
-                _this.height = window._h;
-                _this.width = window._w;
-                delay(function () {
-                    _this.animating = false;
-                },500);
-            })
-        }
-    }
-
-    this.set = function (key,value) {
-        var isResize = ['height','width'].indexOf(key)>=0;
-        if(isResize){
-            this.maximized = false;
-
-            if(this.resizable){
-                this[key] = value;
-            }
-        }else{
+        if(this.resizable){
             this[key] = value;
         }
+    }else{
+        this[key] = value;
     }
+}
 
+App.prototype.hide = function () {
+    this.show = false;
+    this.actived = false;
+};
+
+App.prototype.close = function () {
+    this.closed = true;
+    $event.emit('app:close',this);
+}
+
+App.prototype.maximize = function () {
+    var _this = this;
+    if(_this.maximized && _this.oldPosition){
+        _this.animating = true;
+        delay(function () {
+            _this.maximized = false;
+
+            _this.top = _this.oldPosition.top;
+            _this.left = _this.oldPosition.left;
+            _this.height = _this.oldPosition.height;
+            _this.width = _this.oldPosition.width;
+
+            _this.oldPosition = null;
+            delay(function () {
+                _this.animating = false;
+            },500)
+        })
+
+    }else{
+        _this.animating = true;
+        delay(function () {
+            _this.maximized = true;
+            _this.oldPosition = {
+                top:_this.top,
+                left:_this.left,
+                height:_this.height,
+                width:_this.width
+            };
+            _this.top = 0;
+            _this.left = 0;
+            _this.height = window._h;
+            _this.width = window._w;
+            delay(function () {
+                _this.animating = false;
+            },500);
+        })
+    }
 }
 
 module.exports = {
